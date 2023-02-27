16a5f4089fb7347ceb017a9d26d1b8fcbc29f277
everduvan <everduvanhdez@gmail.com>
2023-02-27 16:53:48 -0500

    update

--- test.py
+++ test.py
@@ -1,16 +1,10 @@
-import pandas as pd 
+import pandas as pd
+import datetime
 
-# dataframe1
-df1 = pd.DataFrame({'col1': [4, 2, 3,'A','C'], 'col2': [4, 5, 6,'B','D'] , 'col3': [1, 2, 3, 4, 5], 'valor': [1, 2, 3,'A','C']})
-print (df1)
-# dataframe2
-df2 = pd.DataFrame({'col1': [1, 2, 3, 4,'D'], 'col2': [4, 5, 6, 7,'D'], 'col3': [1, 2, 3,'A','C']})
-print (df2)
-# Comprobación de coincidencia y asignación de nueva columna
-#df2['coincidencia'] = df2[['columna1', 'columna2']].isin(df1[['columna1', 'columna2']]).all(axis=1).astype(int)
+now = datetime.datetime.now()
+data = {'fecha': [now.strftime('%Y-%m-%d')],
+        'hora': [now.strftime('%H:%M:%S')]}
+df = pd.DataFrame(data)
+print(df)
 
-merged_df = pd.merge(df2, df1[['col1', 'col2', 'valor']], on=['col1', 'col2'], how='left')
-print(merged_df)
 
-merged_df = merged_df.drop(['col3'], axis=1)
-print (merged_df)
