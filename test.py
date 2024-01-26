ae48a8b25a56f6e34243c7ff87a5b24f35972805
everduvan <everduvanhdez@gmail.com>
2024-01-26 15:25:53 -0500

    update

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+import json
 import pandas as pd
 from sqlalchemy.engine import create_engine
 from dotenv import load_dotenv
@@ -11,38 +12,10 @@ data = {'fecha': [now.strftime('%Y-%m-%d')],
 df = pd.DataFrame(data)
 print(df)
 
-def get_df(db: str, table, column=[]):
-    """
-    [get dataframe from postgres]
-    return: pandas-df
-    """
-    a = eval(os.getenv("DB"))
-    db=a[db]
-    engine = create_engine('postgresql+psycopg2://{}:{}@{}:{}/{}'.format
-                           (db['DB_USER'], 
-                            db['DB_PASS'], 
-                            db['DB_IP'], 
-                            db['DB_PORT'], 
-                            db['DB_NAME']))
-    query= f'SELECT * from {table}'
-    df = pd.read_sql_query(query, engine)
-    if column != []:
-        df=df[column]
-    return df
 
-def get_xlsx_df(table):
-      if table != '':
-        df = pd.read_excel(table, engine='openpyxl', na_filter = False)
-      else:
-        print('File not found')
-        df = pd.DataFrame()
-      return df
+# Abre el archivo JSON
+with open('cred.json', 'r') as f:
+    data = json.load(f)
 
-if __name__ == '__main__':
-    df = get_xlsx_df('filtro.xlsx')
-    print (df)
-
-    lista = ['COLOMBIA', '3311']
-    #df_filtrado = df[df['PAIS'].isin(lista)]
-    df_filtrado = df[df.apply(lambda row: row.astype(str).str.contains('|'.join(lista)).any(), axis=1)]
-    print (f'filtrado: \n {df_filtrado}')
+# Ahora puedes acceder a los datos en el diccionario
+print(data["dev"]["DB_USER"])  # Salida: everduvan
